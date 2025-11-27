6e4a037942c9d6015474f29feae69059bdb0ae45
zeelPatel <patelzeel424@gmail.com>
2025-11-27 12:42:32 +0530

    added app.py and Dockerfile

---
+++ app.py
@@ -0,0 +1,17 @@
+import time
+
+def main():
+    print("======================================")
+    print("   Hello from Python inside Docker!   ")
+    print("   This app was built by Jenkins :)   ")
+    print("======================================")
+    
+    # Simulate small runtime
+    for i in range(5):
+        print(f"Running step {i+1}/5...")
+        time.sleep(1)
+
+    print("Done! Pipeline is working successfully.")
+
+if __name__ == "__main__":
+    main()
